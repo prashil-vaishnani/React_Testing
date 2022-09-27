8b12ed8a97a896e9a72cf6939ae6ca243194091f
prashil vaishnani <prashildvaishnani@gmail.com>
2022-09-27 11:25:48 +0530

    different Filename Conventions

--- src/components/greet/Greet.spec.js
+++ src/components/greet/Greet.spec.js
@@ -2,13 +2,13 @@ import { render, screen } from "@testing-library/react";
 import { Greet } from "./Greet";
 
 describe("Greet", () => {
-  test("Greeting sholud be visible", () => {
+  xit("Greeting sholud be visible", () => {
     render(<Greet />);
     const textElement = screen.getByText(/hello/i);
     expect(textElement).toBeInTheDocument();
   });
 
-  test("Greeting to Prashil", () => {
+  fit("Greeting to Prashil", () => {
     render(<Greet name="Prashil" />);
     const textElement = screen.getByText("Hello Prashil");
     expect(textElement).toBeInTheDocument();
