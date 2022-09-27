cf6b074c4720af555cdb283541a5bf784713b226
prashil vaishnani <prashildvaishnani@gmail.com>
2022-09-27 10:45:51 +0530

    Test Driven Development

---
+++ src/components/greet/Greet.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const Greet = ({ name }) => {
+  return <div>Hello {name}</div>;
+};
---
+++ src/components/greet/Greet.test.js
@@ -0,0 +1,14 @@
+import { render, screen } from "@testing-library/react";
+import { Greet } from "./Greet";
+
+test("Greeting sholud be visible", () => {
+  render(<Greet />);
+  const textElement = screen.getByText(/hello/i);
+  expect(textElement).toBeInTheDocument();
+});
+
+test("Greeting to Prashil", () => {
+  render(<Greet name="Prashil" />);
+  const textElement = screen.getByText("Hello Prashil");
+  expect(textElement).toBeInTheDocument();
+});
