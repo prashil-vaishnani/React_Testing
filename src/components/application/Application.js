7958f1eda5760b0cf9bc74a8a4c3e4b28ea6e93e
prashil vaishnani <prashildvaishnani@gmail.com>
2022-09-29 10:26:27 +0530

    getByRole with options(name, lavel,checked,hidden,selected,pressed)

--- src/components/application/Application.js
+++ src/components/application/Application.js
@@ -1,26 +1,34 @@
 export const Application = () => {
   return (
-    <form>
-      <div>
-        <label htmlFor="name">Name</label>
-        <input id="name" type="text" />
-      </div>
-      <div>
-        <label htmlFor="job-location">Job Location</label>
-        <select id="job-location">
-          <option value="">--select country--</option>
-          <option value="US">USA</option>
-          <option value="IND">India</option>
-          <option value="CA">canada</option>
-          <option value="AU">australia</option>
-        </select>
-      </div>
-      <div>
-        <label>
-          <input type="checkbox" id="terms" />I agree
-        </label>
-      </div>
-      <button>submit</button>
-    </form>
+    <>
+      <h1>Job description Form</h1>
+      <h2>section 1</h2>
+      <form>
+        <div>
+          <label htmlFor="name">Name</label>
+          <input id="name" type="text" />
+        </div>
+        <div>
+          <label htmlFor="bio">Bio</label>
+          <textarea id="bio" name="bio" />
+        </div>
+        <div>
+          <label htmlFor="job-location">Job Location</label>
+          <select id="job-location">
+            <option value="">--select country--</option>
+            <option value="US">USA</option>
+            <option value="IND">India</option>
+            <option value="CA">canada</option>
+            <option value="AU">australia</option>
+          </select>
+        </div>
+        <div>
+          <label>
+            <input type="checkbox" id="terms" />I agree
+          </label>
+        </div>
+        <button>submit</button>
+      </form>
+    </>
   );
 };
--- src/components/application/Application.test.js
+++ src/components/application/Application.test.js
@@ -1,11 +1,34 @@
-import { buildQueries, render, screen } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import { Application } from "./Application";
 
 describe("Application", () => {
   test("rander correctly", () => {
     render(<Application />);
-    const nameElement = screen.getByRole("textbox");
+
+    const heading = screen.getByRole("heading", {
+      name: "Job description Form",
+    });
+    expect(heading).toBeInTheDocument();
+
+    const headingByLabel = screen.getByRole("heading", {
+      level: 1,
+    });
+    expect(headingByLabel).toBeInTheDocument();
+
+    const sectionHeading = screen.getByRole("heading", {
+      name: "section 1",
+    });
+    expect(sectionHeading).toBeInTheDocument();
+
+    const nameElement = screen.getByRole("textbox", {
+      name: "Name",
+    });
     expect(nameElement).toBeInTheDocument();
+
+    const bioElement = screen.getByRole("textbox", {
+      name: "Bio",
+    });
+    expect(bioElement).toBeInTheDocument();
 
     const jobLocationElement = screen.getByRole("combobox");
     expect(jobLocationElement).toBeInTheDocument();
