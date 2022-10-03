2b749aa74ad5d99bceece575cbe993c0f6c7911e
prashil vaishnani <prashildvaishnani@gmail.com>
2022-10-03 14:42:11 +0530

    set input value as counter value

--- src/components/counter/Counter.js
+++ src/components/counter/Counter.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 
 export const Counter = () => {
   const [count, setCount] = useState(0);
+  const [amount, setAmount] = useState(0);
   return (
     <div>
       <h1>{count}</h1>
@@ -12,6 +13,13 @@ export const Counter = () => {
       >
         +
       </button>
+      <input
+        name="amount"
+        type="number"
+        value={amount}
+        onChange={(e) => setAmount(parseInt(e.target.value))}
+      />
+      <button onClick={() => setCount(amount)}>set</button>
     </div>
   );
 };
--- src/components/counter/Counter.test.js
+++ src/components/counter/Counter.test.js
@@ -3,6 +3,7 @@ import user from "@testing-library/user-event";
 import { Counter } from "./Counter";
 
 describe("Counter", () => {
+  /*component render check */
   test("render correctly", () => {
     render(<Counter />);
 
@@ -15,12 +16,14 @@ describe("Counter", () => {
     expect(buttonElement).toBeInTheDocument();
   });
 
+  /* initial value check */
   test("render count 0", () => {
     render(<Counter />);
     const countElement = screen.getByRole("heading");
     expect(countElement).toHaveTextContent("0");
   });
 
+  /*onclick increment value check */
   test("reander 1 after clicking button for first times", async () => {
     user.setup();
     render(<Counter />);
@@ -41,5 +44,36 @@ describe("Counter", () => {
     await user.dblClick(buttonElement);
     const countElement = screen.getByRole("heading");
     expect(countElement).toHaveTextContent("2");
+  });
+
+  /*keyboard event check */
+  test("keyboard key change handler", async () => {
+    user.setup();
+    render(<Counter />);
+    const amountElement = screen.getByRole("spinbutton");
+    await user.type(amountElement, "10");
+    expect(amountElement).toHaveValue(10);
+
+    const setButton = screen.getByRole("button", {
+      name: "set",
+    });
+    await user.click(setButton);
+    const headingValue = screen.getByRole("heading");
+    expect(headingValue).toHaveTextContent("10");
+  });
+
+  test("30 as an output", async () => {
+    user.setup();
+    render(<Counter />);
+    const amountElement = screen.getByRole("spinbutton");
+    await user.type(amountElement, "30");
+    expect(amountElement).toHaveValue(30);
+
+    const setAmount = screen.getByRole("button", {
+      name: "set",
+    });
+    await user.click(setAmount);
+    const headingValue = screen.getByRole("heading");
+    expect(headingValue).toHaveTextContent("30");
   });
 });
